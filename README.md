dffe021f6e56825e8e4a27eefba8371f1a22a2ba
Zaid <zaid.naom@gmail.com>
2019-07-28 20:30:35 +0200

    Update Fable compiler and dependencies, use Feliz

--- README.md
+++ README.md
@@ -4,9 +4,20 @@ This is a simple Fable app including an [Elmish](https://elmish.github.io/) coun
 
 ## Building and running the app
 
+First of all, start with installing the project's npm dependencies
 ```bash
 npm install
+```
+Once this is finished, you can then build and compile the project:
+```
+npm run build
+```
+You can start developing the application in watch mode using the webpack development server:
+```
 npm start
 ```
+After the first compilation is finished, navigate to http://localhost:8080 in your browser to see the application.
 
-After the first compilation is finished, in your browser open: http://localhost:8080/
+### VS Code
+
+If you happen to use Visual Studio Code, simply hitting F5 will start the development watch mode for you and opens your default browser navigating to http://localhost:8080.
--- src/App.fs
+++ src/App.fs
@@ -2,35 +2,44 @@ module App
 
 open Elmish
 open Elmish.React
-open Fable.React 
-open Fable.React.Props
+open Feliz
 
-type State = { Count: int }
+type State =
+    { Count: int }
 
 type Msg =
-  | Increment
-  | Decrement
-  
-let init() = { Count = 0 }
+    | Increment
+    | Decrement
 
-let update (msg: Msg) (currentState: State) =
-  match msg with
-  | Increment -> 
-      let nextState = { currentState with Count = currentState.Count + 1 }
-      nextState
+let init() =
+    { Count = 0 }
 
-  | Decrement -> 
-      let nextState = { currentState with Count = currentState.Count - 1 }
-      nextState
-      
+let update (msg: Msg) (state: State): State =
+    match msg with
+    | Increment ->
+        { state with Count = state.Count + 1 }
+
+    | Decrement ->
+        { state with Count = state.Count - 1 }
+
 let render (state: State) (dispatch: Msg -> unit) =
-  div [ ] [ 
-    button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
-    div [ ] [ str (string state.Count) ]
-    button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] 
-  ]
+    Html.div [
+        prop.id "counter"
+        prop.children [
+            Html.button [
+                prop.onClick (fun _ -> dispatch Increment)
+                prop.children [ Html.text "Decrement" ]
+            ]
+
+            Html.button [
+                prop.onClick (fun _ -> dispatch Decrement)
+                prop.children [ Html.text "Decrement" ]
+            ]
+
+            Html.h1 state.Count
+        ]
+    ]
 
 Program.mkSimple init update render
 |> Program.withReactSynchronous "elmish-app"
-|> Program.withConsoleTrace
 |> Program.run
