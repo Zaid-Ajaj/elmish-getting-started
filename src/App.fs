7f374c759c4cd14a124c99e0eacb2b2eb8febb16
Zaid <zaid.naom@gmail.com>
2019-08-31 21:09:36 +0200

    Update Feliz to 0.46 and Fable to 2.3.20

--- src/App.fs
+++ src/App.fs
@@ -23,22 +23,19 @@ let update (msg: Msg) (state: State): State =
         { state with Count = state.Count - 1 }
 
 let render (state: State) (dispatch: Msg -> unit) =
-    Html.div [
-        prop.id "counter"
-        prop.children [
-            Html.button [
-                prop.onClick (fun _ -> dispatch Increment)
-                prop.text "Increment"
-            ]
-
-            Html.button [
-                prop.onClick (fun _ -> dispatch Decrement)
-                prop.text "Decrement"
-            ]
-
-            Html.h1 state.Count
-        ]
+  Html.div [
+    Html.button [
+      prop.onClick (fun _ -> dispatch Increment)
+      prop.text "Increment"
     ]
+
+    Html.button [
+      prop.onClick (fun _ -> dispatch Decrement)
+      prop.text "Decrement"
+    ]
+
+    Html.h1 state.Count
+  ]
 
 Program.mkSimple init update render
 |> Program.withReactSynchronous "elmish-app"
