ba7f6162b78efca669c707df1c3bbf175e84a22c
Andrzej Kowalewski <andrzej.kowalewski98@gmail.com>
2021-06-28 00:57:17 +0200

    added hot module reloading and typed css

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,50 @@
 const path = require("path")
+const DotenvPlugin = require("dotenv-webpack")
+var webpack = require("webpack");
 
-module.exports = {
-    mode: "none",
-    entry: "./src/App.fsproj",
-    devServer: {
-        contentBase: path.join(__dirname, "./dist")
-    },
-    module: {
-        rules: [{
-            test: /\.fs(x|proj)?$/,
-            use: "fable-loader"
-        }]
+module.exports = (env, argv) => {
+    // extract build mode from command-line
+    const mode = argv.mode
+    console.log(mode);
+
+    return {
+        mode: mode,
+        entry: "./src/App.fsproj",
+        devServer: {
+            contentBase: path.join(__dirname, "./dist"),
+            hot: true,
+            inline: true
+        },
+        plugins: mode === "development" ?
+            // development mode plugins
+            [
+                new DotenvPlugin({
+                    path: path.join(__dirname, ".env"),
+                    silent: true,
+                    systemvars: true
+                }),
+
+                new webpack.HotModuleReplacementPlugin()
+            ]
+            :
+            // production mode plugins
+            [
+                new DotenvPlugin({
+                    path: path.join(__dirname, ".env"),
+                    silent: true,
+                    systemvars: true
+                })
+            ],
+        module: {
+            rules: [{
+                test: /\.fs(x|proj)?$/,
+                use: {
+                    loader: "fable-loader",
+                    options: {
+                        define: mode === "development" ? ["DEVELOPMENT"] : []
+                    }
+                }
+            }]
+        }
     }
 }
